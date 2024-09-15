1746eddc4a7f1bc1f29f687a3a935814275c9463
karishmarout05 <133682894+karishmarout05@users.noreply.github.com>
2024-09-15 13:17:24 +0530

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [ "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: [ "Developer", "UI/UX designer", "Front-end developer",  "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
